9fa3c8d97c1dfa13d3222f9cdbf394ae2f0044fa
Weiliang Huang <whua0055@student.monash.edu>
2024-10-11 18:38:14 +1100

    finalise things

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 import EHeader from './components/EHeader.vue'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+const showHeader = computed(() => {
+  return route.name !== 'GetAllEventsAPI' && route.name !== 'EventCountAPI'
+})
 </script>
 
 <template>
   <div class="container">
     <div class="row">
-      <header>
+      <header v-if="showHeader">
         <EHeader />
       </header>
     </div>
